55270d576cf1b98361760bd3156074bc9afb1de5
Eirv <2114486405@qq.com>
2025-12-21 13:54:55 +0800

    Refactor Arch enum and is_arch structure

--- lsplant/src/main/jni/include/utils/type_traits.hpp
+++ lsplant/src/main/jni/include/utils/type_traits.hpp
@@ -13,14 +13,17 @@ template <class T, template <class, class...> class U>
 inline constexpr bool is_instance_v = is_instance<T, U>::value;
 
 enum class Arch {
-    kUnknown,
+    kUnknown = 0,
+
     kArm64,
     kArm,
     kX86,
     kX64,
     kRiscv64,
-    kLP64,
-    kLP32,
+
+    kLP32 = -32,
+    kLP64 = -64,
+
 #if defined(__aarch64__)
     kCurrent = kArm64,
 #elif defined(__arm__)
@@ -34,6 +37,7 @@ enum class Arch {
 #else
     kCurrent = kUnknown,
 #endif
+
 #if defined(__LP64__)
     kLPCurrent = kLP64,
 #else
@@ -45,12 +49,11 @@ template <Arch...>
 struct is_arch;
 
 template <Arch kArch>
-struct is_arch<kArch>
-    : std::integral_constant<bool, kArch == Arch::kCurrent || kArch == Arch::kLPCurrent> {};
+struct is_arch<kArch> : std::bool_constant<kArch == Arch::kCurrent || kArch == Arch::kLPCurrent> {};
 
 template <Arch kFirst, Arch... kRest>
 struct is_arch<kFirst, kRest...>
-    : std::integral_constant<bool, is_arch<kFirst>::value || is_arch<kRest...>::value> {};
+    : std::bool_constant<is_arch<kFirst>::value || is_arch<kRest...>::value> {};
 
 template <Arch... kArchs>
 inline constexpr bool is_arch_v = is_arch<kArchs...>::value;
